decdbee2fbf04749613e0f93e1d75616b07d54fe
eugeneborodin <eugeneborodin@mail.ru>
2017-02-06 00:58:11 +0300

    init

---
+++ AOUT.CH3.LogAn2/LogAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿namespace AOUT.CH3.LogAn2
+{
+    public interface IExtensionManager
+    {
+        bool IsValid(string fileName);
+    }
+
+    public class FileExtensionManager : IExtensionManager
+    {
+        public bool IsValid(string fileName)
+        {
+            return true;
+            // здесь читается файл                   
+        }
+    }
+
+    public class AlwaysValidFakeExtensionManager : IExtensionManager
+    {
+        public bool IsValid(string fileName)
+        {
+            return true;
+        }
+    }
+
+    public class LogAnalyzer
+    {
+        private IExtensionManager manager;
+
+        public LogAnalyzer(IExtensionManager mgr)
+        {
+            manager = mgr;
+        }        
+
+        public bool IsValidLogFileName(string fileName)
+        {           
+            return manager.IsValid(fileName);
+        }       
+    }    
+}
